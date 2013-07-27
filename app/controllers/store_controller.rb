5b950e84796b94bff9e7a17596a7ed7d1e74dc57
Alex Emelyanov <holyketzer@gmail.com>
2013-07-27 23:36:53 +0400

    Added user visit counter for store#index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	@visit_count = increment_visit_count
   end
+
+  private 
+  	def increment_visit_count
+  		session[:counter] = 0 if session[:counter].nil?
+  		session[:counter] += 1
+  	end
 end
