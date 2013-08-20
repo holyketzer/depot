8bc2f442c9f56936aa21c4cc4bb13f9f3a731ef5
Alex Emelyanov <holyketzer@gmail.com>
2013-08-20 12:44:04 +0400

    Added email notification for users.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,17 @@ Depot::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "",
+    authentication: "plain",
+    user_name: "",
+    password: "",
+    enable_starttls_auto: true
+  }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
