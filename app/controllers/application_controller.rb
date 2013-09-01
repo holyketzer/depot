a0ea208fa3e6bda5dda4a21f33e33f1c0987f6c1
Alex Emelyanov <holyketzer@gmail.com>
2013-09-01 12:09:09 +0400

    Added basic HTTP authenication for not Mime:HTTP queries.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,16 @@ class ApplicationController < ActionController::Base
   before_action :authorize
 
   protected
-  	def authorize
-  		unless User.find_by(id: session[:user_id])
-  			redirect_to login_url, notice: "Please log in"
-  		end
+  	def authorize    
+      unless request.format == Mime::HTML
+        authenticate_or_request_with_http_basic do |name,password|
+          user = User.find_by_name(name)
+          session[:user_id] = user.id if user && user.authenticate(password)
+        end  
+      else
+    		unless User.find_by(id: session[:user_id])
+  			  redirect_to login_url, notice: "Please log in"  		
+        end
+      end
   	end
 end
